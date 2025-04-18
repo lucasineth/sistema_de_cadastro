9b7023d2c7f4770e7c5c4903bc0c0cc76cb48483
lucasineth <lucasramos1418@gmail.com>
2025-04-18 14:19:20 -0300

    adding new function of adit and remove user and salve all in json

--- cadastro.rb
+++ cadastro.rb
@@ -1,5 +1,7 @@
+require 'json'
+
 class Usuario
-  attr_reader :nome, :email, :idade
+  attr_accessor :nome, :email, :idade
 
   def initialize(nome, email, idade)
     @nome = nome
@@ -10,30 +12,80 @@ class Usuario
   def exiber_dados
     puts "Nome: #{@nome} | Email: #{@email} | Idade: #{@idade}"
   end
+
+  def to_h
+    { nome: @nome, email: @email, idade: @idade }
+  end
+
+  def self.from_hash(hash)
+    Usuario.new(hash["nome"], hash["email"], hash["idade"])
+  end
 end
 
 class SistemaCadastro
+  ARQUIVO = "usuarios.json"
+
   def initialize
-    @usuarios = []
+    @usuarios = carregar_usuarios
   end
 
   def adicionar_usuario(usuario)
     @usuarios << usuario
+    salvar_usuarios
     puts "Usuário '#{usuario.nome}' cadastrado com sucesso!"
   end
 
   def listar_usuarios
     puts "\n-=-=-=- Lista de Usuário -=-=-=-"
-    @usuarios.each { |u| u.exiber_dados }
+    @usuarios.each_with_index do |u, i| 
+      puts "[#{i + 1}]"
+      u.exiber_dados 
+    end
   end
 
   def buscar_usuario(nome)
-    resultado = @usuarios.find { |u| u.nome.downcase == nome.downcase }
-    if resultado
-      puts "\nUsuário encontroda:"
-      resultado.exiber_dados
+    @usuarios.find { |u| u.nome.downcase == nome.downcase }
+  end
+
+  def editar_usuario(nome)
+    usuario = buscar_usuario(nome)
+    if usuario
+      print "Novo nome (ou enter para manter '#{usuario.nome}'): "
+      novo_nome = gets.chomp
+      usuario.nome = novo_nome unless novo_nome.empty?
+
+      print "Novo emnail (ou enter para manter '#{usuario.email}'): "
+      novo_email = gets.chomp
+      usuario.email = novo_email unless novo_email.empty?
+
+      print "Novo idade (ou enter para manter '#{usuario.idade}'): "
+      novo_idade = gets.chomp
+      usuario.idade = novo_idade unless novo_idade.empty?
+
+      salvar_usuarios
+      puts "Usuário atualizado com sucesso!"
     else
-      puts "Usuário '#{nome}' não encontrado."
+      puts "usuário '#{nome}' não encontrado."
+    end
+  end
+
+  def remover_usuario(nome)
+    usuario = buscar_usuario(nome)
+    if usuario
+      @usuarios.delete(Usuario)
+      salvar_usuarios
+      puts "Usuário '#{nome}' removido com sucesso!"
+    else
+      puts "Usuário '#{nome}' não encontrado"
+    end
+  end
+
+  def carregar_usuarios
+    if File.exist?(ARQUIVO)
+      dados = JSON.parse(File.read(ARQUIVO))
+      dados.map { |hash| Usuario.from_hash(hash) }
+    else
+      []
     end
   end
 end
